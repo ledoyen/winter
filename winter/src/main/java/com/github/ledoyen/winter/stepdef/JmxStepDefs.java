eda04b26f095bab48fe1f0aebc0b270979e17f21
ledoyen <ledoyen.loic@gmail.com>
2016-11-11 23:12:42 +0100

    add JMX step defs draft

---
+++ winter/src/main/java/com/github/ledoyen/winter/stepdef/JmxStepDefs.java
@@ -0,0 +1,24 @@
+package com.github.ledoyen.winter.stepdef;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.ledoyen.automocker.extension.jmx.LocalMBeans;
+
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+
+public class JmxStepDefs {
+
+	private final Map<String, Map<String, Object>> recordedMBeanAttributesByObjectNames = new HashMap<>();
+
+	@Given("JMX bean (.+) is recorded")
+	public void jmx_bean_is_recorded(String name) {
+		recordedMBeanAttributesByObjectNames.put(name, LocalMBeans.INSTANCE.listAttributes(name));
+	}
+
+	@After
+	public void afterScenario() {
+		recordedMBeanAttributesByObjectNames.clear();
+	}
+}
