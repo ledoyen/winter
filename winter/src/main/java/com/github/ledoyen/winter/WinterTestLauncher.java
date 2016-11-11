2a7186057a111db05952b8061ab3e3911d23fec8
ledoyen <ledoyen.loic@gmail.com>
2016-11-11 14:32:52 +0100

    add strict and display (pretty) default Cucumber options

--- winter/src/main/java/com/github/ledoyen/winter/WinterTestLauncher.java
+++ winter/src/main/java/com/github/ledoyen/winter/WinterTestLauncher.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 
 @RunWith(WinterJUnit4FeatureRunner.class)
-@CucumberOptions(features = "classpath:features")
+@CucumberOptions(features = "classpath:features", plugin = "pretty", strict = true)
 public class WinterTestLauncher {
 
 }
