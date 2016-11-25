a5396e0de881f81dc9f493c3f07206150eb8e602
ledoyen <ledoyen.loic@gmail.com>
2016-11-25 01:11:19 +0100

    add debug step defitions "nothing"

--- winter/src/main/java/com/github/ledoyen/winter/stepdef/AnchorStepDef.java
+++ winter/src/main/java/com/github/ledoyen/winter/stepdef/AnchorStepDef.java
@@ -1,9 +1,25 @@
 package com.github.ledoyen.winter.stepdef;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 /**
  * The sole purpose of this class is to anchor the stepdef package in case of refactoring.
  */
 public final class AnchorStepDef {
 	private AnchorStepDef() {
+	}
+
+	@Given("nothing")
+	public void given_nothing() {
+	}
+
+	@When("nothing")
+	public void when_nothing() {
+	}
+
+	@Then("nothing")
+	public void then_nothing() {
 	}
 }
