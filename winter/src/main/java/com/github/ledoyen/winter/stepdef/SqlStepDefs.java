c874790c35d026a4b79ef6996c0c11cd4b551e7f
ledoyen <ledoyen.loic@gmail.com>
2016-11-25 01:09:53 +0100

    remove DataSource reference from SQL step definitions

--- winter/src/main/java/com/github/ledoyen/winter/stepdef/SqlStepDefs.java
+++ winter/src/main/java/com/github/ledoyen/winter/stepdef/SqlStepDefs.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 import java.util.List;
 
 import javax.inject.Provider;
-import javax.sql.DataSource;
 
 import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,25 +24,26 @@ public class SqlStepDefs {
 	@Autowired
 	private Provider<DatasourceLocator> datasourceLocator;
 
-	private final ListMultimap<DataSource, String> tablesToClean = ArrayListMultimap.create();
+	private final ListMultimap<String, String> tablesToClean = ArrayListMultimap.create();
 
 	@Given("^an SQL table (\\S+) containing$")
 	public void a_call_on_service_method_is_made(String tableName, DataTable data) throws Exception {
 		DataSources.populateTable(datasourceLocator.get()
 				.getDataSource(), tableName, data.topCells(), data.cells(1));
 		tablesToClean.put(datasourceLocator.get()
-				.getDataSource(), tableName);
+				.getName(), tableName);
 	}
 
 	@After
 	public void afterScenario() {
 		tablesToClean.asMap()
-				.forEach((ds, names) -> DataSources.doInConnection(ds, c -> {
-					// TODO fix this, not working
-					List<String> copy = Lists.newArrayList(names);
-					Collections.sort(copy, Comparator.reverseOrder());
-					names.forEach(name -> Connections.truncate(c, name));
-				}));
+				.forEach((dsName, names) -> DataSources.doInConnection(datasourceLocator.get()
+						.getDataSource(dsName), c -> {
+							// TODO fix this, not working
+							List<String> copy = Lists.newArrayList(names);
+							Collections.sort(copy, Comparator.reverseOrder());
+							names.forEach(name -> Connections.truncate(c, name));
+						}));
 		tablesToClean.clear();
 	}
 }
